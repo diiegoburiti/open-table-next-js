9683b7e606ac940e19b85c5f94f9d77637df1a1d
Diego Buriti <bs.diego97@gmail.com>
2023-04-11 14:14:37 -0300

    refactor: adding header component

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,23 @@
+import { SearchBar } from 'components/searchBar'
+
+export type HeaderProps = {
+  hiddenTitle?: boolean
+}
+
+export const Header = ({ hiddenTitle = false }: HeaderProps) => (
+  <div
+    className={
+      'bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2 ' +
+      (hiddenTitle ? 'h-auto' : 'h-64')
+    }
+  >
+    <div className={'text-center ' + (hiddenTitle ? 'mt-0' : 'mt-10')}>
+      {!hiddenTitle && (
+        <h1 className={'text-white text-5xl font-bold mb-2'}>
+          Find your table for any occasion
+        </h1>
+      )}
+      <SearchBar />
+    </div>
+  </div>
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { NavBar } from 'components/navBar'
+import { Header } from 'components/header'
 
 export default function Page() {
   return (
@@ -7,27 +8,8 @@ export default function Page() {
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
         <main>
-          {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              {/* SEARCH BAR */}
-              <div className="text-left text-lg py-3 m-auto flex justify-center">
-                <input
-                  className="rounded  mr-3 p-2 w-[450px]"
-                  type="text"
-                  placeholder="State, city or town"
-                />
-                <button className="rounded bg-red-600 px-9 py-2 text-white">
-                  {"Let's go"}
-                </button>
-              </div>
-              {/* SEARCH BAR */}
-            </div>
-          </div>
-          {/* HEADER */} {/* CARDS */}
+          <Header />
+          {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
             {/* CARD */}
             <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
