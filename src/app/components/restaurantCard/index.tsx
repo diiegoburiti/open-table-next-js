f8619f2005e032a380fdfb8925c72ce5d1db0c68
Diego Buriti <bs.diego97@gmail.com>
2023-04-11 14:43:55 -0300

    refactor: adding a prop to aling restaurant card component in row or column

--- src/app/components/restaurantCard/index.tsx
+++ src/app/components/restaurantCard/index.tsx
@@ -1,27 +1,61 @@
 import Link from 'next/link'
+export type RestaurantCardProps = {
+  cardStyle?: 'row' | 'column'
+}
 
-export const RestaurantCard = () => (
-  <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-    {/* TODO: LEAVE IT DYNAMICALLY */}
-    <Link href="/restaurant/lorem-ipsum">
-      <img
-        src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-        alt=""
-        className="w-full h-36"
-      />
-      <div className="p-1">
-        <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-        <div className="flex items-start">
-          <div className="flex mb-2">*****</div>
-          <p className="ml-2">77 reviews</p>
+export const RestaurantCard = ({ cardStyle }: RestaurantCardProps) => {
+  return (
+    <>
+      {cardStyle === 'column' ? (
+        <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
+          {/* TODO: LEAVE IT DYNAMICALLY */}
+          <Link href="/restaurant/lorem-ipsum">
+            <img
+              src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
+              alt=""
+              className="w-full h-36"
+            />
+            <div className="p-1">
+              <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
+              <div className="flex items-start">
+                <div className="flex mb-2">*****</div>
+                <p className="ml-2">77 reviews</p>
+              </div>
+              <div className="flex text-reg font-light capitalize">
+                <p className=" mr-3">Mexican</p>
+                <p className="mr-3">$$$$</p>
+                <p>Toronto</p>
+              </div>
+              <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
+            </div>
+          </Link>
         </div>
-        <div className="flex text-reg font-light capitalize">
-          <p className=" mr-3">Mexican</p>
-          <p className="mr-3">$$$$</p>
-          <p>Toronto</p>
+      ) : (
+        <div className="border-b flex pb-5">
+          <img
+            src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
+            alt=""
+            className="w-44 rounded"
+          />
+          <div className="pl-5">
+            <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+            <div className="flex items-start">
+              <div className="flex mb-2">*****</div>
+              <p className="ml-2 text-sm">Awesome</p>
+            </div>
+            <div className="mb-9">
+              <div className="font-light flex text-reg">
+                <p className="mr-4">$$$</p>
+                <p className="mr-4">Mexican</p>
+                <p className="mr-4">Ottawa</p>
+              </div>
+            </div>
+            <div className="text-red-600">
+              <a href="">View more information</a>
+            </div>
+          </div>
         </div>
-        <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-      </div>
-    </Link>
-  </div>
-)
+      )}
+    </>
+  )
+}
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from 'components/header'
 import { NavBar } from 'components/navBar'
+import { RestaurantCard } from 'components/restaurantCard'
 
 export default function Search() {
   return (
@@ -42,32 +43,7 @@ export default function Search() {
           </div>
           {/* SEARCH SIDE BAR */}
           <div className="w-5/6">
-            {/* RESAURANT CAR */}
-            <div className="border-b flex pb-5">
-              <img
-                src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                alt=""
-                className="w-44 rounded"
-              />
-              <div className="pl-5">
-                <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
-                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
-                  <p className="ml-2 text-sm">Awesome</p>
-                </div>
-                <div className="mb-9">
-                  <div className="font-light flex text-reg">
-                    <p className="mr-4">$$$</p>
-                    <p className="mr-4">Mexican</p>
-                    <p className="mr-4">Ottawa</p>
-                  </div>
-                </div>
-                <div className="text-red-600">
-                  <a href="">View more information</a>
-                </div>
-              </div>
-            </div>
-            {/* RESAURANT CAR */}
+            <RestaurantCard cardStyle="row" />
           </div>
         </div>
       </main>
