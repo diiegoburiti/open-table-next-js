5079c5addbfce194aac65be542d06a4a191b6ef0
Diego Buriti <bs.diego97@gmail.com>
2023-04-12 08:03:26 -0300

    refactor: adding banner component

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,3 +1,4 @@
+import { Banner } from 'components/banner'
 import { NavBar } from 'components/navBar'
 import Link from 'next/link'
 
@@ -6,15 +7,8 @@ export default function RestaurantMenu() {
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
-        {/* HEADER */}
-        <div className="h-96 overflow-hidden">
-          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-            <h1 className="text-7xl text-white captitalize text-shadow text-center">
-              Milestones Grill (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* HEADER */} {/* DESCRIPTION PORTION */}
+        <Banner />
+        {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[100%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,20 +1,13 @@
-import { NavBar } from 'components/navBar'
 import Link from 'next/link'
+import { Banner } from 'components/banner'
+import { NavBar } from 'components/navBar'
 
 export default function RestaurantDetails() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
-        {/* HEADER */}
-        <div className="h-96 overflow-hidden">
-          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-            <h1 className="text-7xl text-white captitalize text-shadow text-center">
-              Milestones Grill (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* HEADER */} {/* DESCRIPTION PORTION */}
+        <Banner /> {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[70%] rounded p-3 shadow">
             {/* RESTAURANT NAVBAR */}
